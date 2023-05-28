d1fdc000e17cc714d60841dbca8ea6dd0bfb2d0a
SafiRashidi <safirashidi55@gmail.com>
2023-05-28 10:37:49 -0500

    API classes added

--- src/test/java/TestRunner/APIRunner.java
+++ src/test/java/TestRunner/APIRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features",
         glue = "APIStepDefinitions",
         dryRun = false,
-        tags = "@apijsonworkflow",
+        tags = "@updateemployee",
         plugin = {"pretty", "html:target/Cucumber.html", "json:target/Cucumber.json", "rerun:target/failed.txt" }
 )
 
